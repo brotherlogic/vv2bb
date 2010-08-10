3d94005b07ad7c6492875b0b36060ae198707fd1
Simon Tucker <s.tucker@dcs.shef.ac.uk>
2010-08-10 18:17:16 +0100

    Addressing findbugs bugs

--- src/main/java/uk/co/brotherlogic/forum/TransferProperties.java
+++ src/main/java/uk/co/brotherlogic/forum/TransferProperties.java
@@ -5,25 +5,22 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class TransferProperties {
-	
+
 	static Properties props;
-	
-	public static String getProperty(String key)
-	{
+
+	public static String getProperty(String key) {
 		if (props == null)
 			buildProps();
 		return props.getProperty(key);
 	}
-	
-	private static void buildProps()
-	{
+
+	private static void buildProps() {
 		props = new Properties();
-		try
-		{
-			props.load(new FileInputStream("forum.properties"));
-		}
-		catch (IOException e)
-		{
+		try {
+			FileInputStream fis = new FileInputStream("forum.properties");
+			props.load(fis);
+			fis.close();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
